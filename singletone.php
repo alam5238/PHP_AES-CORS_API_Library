becc51f1603b31cf463a878475036681b67500e0
Nazmul Alam Shuvo <33131389+alam5238@users.noreply.github.com>
2025-05-29 02:11:47 +0600

    singletone.php

--- singletone.php
+++ singletone.php
@@ -1,5 +1,11 @@
 <?php
-
+    /* 
+        This library is Developed By Engr. Nazmul Alam Shuvo
+        MSc & BSc in CSE
+        Email: nazmul.shuvo2@gmail.com
+        Social FB: fb.com/alam5238
+        Don't use it on Unethical purpose.
+    */
     $set_accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7";
     $set_userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
 
